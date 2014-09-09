235d4926f9c672d91d20e61b652d254a9a0eb43b
tflave <franknguyen007@gmail.com>
2014-09-09 14:26:46 +0700

    Fix bug

--- finalProject/finalProject/finalProject/IngameScreen.cs
+++ finalProject/finalProject/finalProject/IngameScreen.cs
@@ -5,8 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using finalProject;
 
-namespace finalProject
+namespace ScreenManager
 {
     public class IngameScreen : GameScreen
     {
